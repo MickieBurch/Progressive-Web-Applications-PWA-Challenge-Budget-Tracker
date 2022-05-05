67fd62b0d994268fc4da79b5475ca159891fc2fb
McKenna <Mckennaburch19@gmail.com>
2022-05-04 20:46:55 -0500

    updated the DB file for this current project

--- symmetrical-bassoon-main/Develop/public/assets/js/db.js
+++ symmetrical-bassoon-main/Develop/public/assets/js/db.js
@@ -1,9 +1,9 @@
 let db;
-const request = indexedDB.open('pizza_hunt', 1);
+const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function(event) {
   const db = event.target.result;
-  db.createObjectStore('new_pizza', { autoIncrement: true });
+  db.createObjectStore('pending', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -12,7 +12,7 @@ request.onsuccess = function(event) {
 
   // check if app is online, if yes run checkDatabase() function to send all local db data to api
   if (navigator.onLine) {
-    uploadPizza();
+    checkDatabase();
   }
 };
 
@@ -22,53 +22,41 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+  const transaction = db.transaction(['pending'], 'readwrite');
 
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+  const store = transaction.objectStore('pending');
 
   // add record to your store with add method.
-  pizzaObjectStore.add(record);
+  const getAll = store.getAll();
 }
 
-function uploadPizza() {
-  // open a transaction on your pending db
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
-
-  // access your pending object store
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
-
-  // get all records from store and set to a variable
-  const getAll = pizzaObjectStore.getAll();
-
-  getAll.onsuccess = function() {
-    // if there was data in indexedDb's store, let's send it to the api server
-    if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
           }
-
-          const transaction = db.transaction(['new_pizza'], 'readwrite');
-          const pizzaObjectStore = transaction.objectStore('new_pizza');
-          // clear all items in your store
-          pizzaObjectStore.clear();
         })
-        .catch(err => {
-          // set reference to redirect back here
-          console.log(err);
-        });
-    }
-  };
-}
+          .then(response => response.json())
+          .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+      }
+    };
+  }
+
+
 
 // listen for app coming back online
-window.addEventListener('online', uploadPizza);
+window.addEventListener('online', checkDatabase);
