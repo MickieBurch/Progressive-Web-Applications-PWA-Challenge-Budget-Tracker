50f618b8b58a1208e2fcdf60b8dcfa421e32ee71
McKenna <Mckennaburch19@gmail.com>
2022-05-04 20:09:01 -0500

    added files to cache

--- symmetrical-bassoon-main/Develop/public/service-worker.js
+++ symmetrical-bassoon-main/Develop/public/service-worker.js
@@ -2,18 +2,12 @@ const APP_PREFIX = 'FoodEvent-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./events.html",
-  "./tickets.html",
-  "./schedule.html",
-  "./assets/css/style.css",
-  "./assets/css/bootstrap.css",
-  "./assets/css/tickets.css",
-  "./dist/app.bundle.js",
-  "./dist/events.bundle.js",
-  "./dist/tickets.bundle.js",
-  "./dist/schedule.bundle.js"
-];
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/assets/js/db.js",
+]
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
