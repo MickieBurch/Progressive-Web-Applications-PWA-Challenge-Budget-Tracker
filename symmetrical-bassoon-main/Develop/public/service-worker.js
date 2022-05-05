cdb12c070362ea8d6c6fb84549c300e15440a5f4
McKenna <Mckennaburch19@gmail.com>
2022-05-04 20:38:12 -0500

    added the ability to log information offline within the service-worker. Changed the code to work for this project. Added files into an assets folder instead of having the files seperately in the public folder. Added the 72x72 icon into the manifest.json list.

--- symmetrical-bassoon-main/Develop/public/service-worker.js
+++ symmetrical-bassoon-main/Develop/public/service-worker.js
@@ -1,34 +1,16 @@
-const APP_PREFIX = 'FoodEvent-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/assets/css/styles.css",
     "/assets/js/index.js",
     "/assets/js/db.js",
+
 ]
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
 
-// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -36,26 +18,52 @@ self.addEventListener('install', function (e) {
       return cache.addAll(FILES_TO_CACHE)
     })
   )
-})
+  self.skipwaiting();
+});
 
-// Delete outdated caches
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      })
-      // add current cache name to keeplist
-      cacheKeeplist.push(CACHE_NAME);
-
       return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
           console.log('deleting cache : ' + keyList[i] );
           return caches.delete(keyList[i]);
         }
       }));
+    })
+  );
+  self.clients.claim();
+});
+
+
+
+self.addEventListener('fetch', function (e) {
+    if(evt.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch(data)', evt.request.url);
+
+
+e.respondWith(
+    caches.open(DATA_CACHE_NAME).then(function (e){
+        return fetch(e.request)
+        .then(response => {
+            if (response.status === 200){
+                cache.put(evt.request.url, response.clone());
+            }
+            return response;
+        })
+        .catch(err => {
+            return cache.match(e.request);
+        });
+    })
+    );
+    return;
+}
+
+e.respondWith(
+    caches.open(CACHE_NAME).then(function (e){
+      return cache.match(e.request).then(response => {
+        return response || fetch(e.request);
+      });
     })
   );
 });
